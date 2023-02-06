2f9071dc57cc0d93754c475c4e3932d63e55f2b2
YunByungil <dbsquddlfz@naver.com>
2023-02-06 17:55:20 +0900

    객체지향 쿼리 언어1 - 기본 문법 완료

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -6,6 +6,9 @@ package hellojpa;
 import org.hibernate.Hibernate;
 
 import javax.persistence.*;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -17,44 +20,16 @@ public class JpaMain {
         EntityTransaction tx = em.getTransaction();
         tx.begin();
         try {
-//            Member member = new Member();
-//            member.setUsername("member1");
-//            member.setHomeAddress(new Address("homeCity", "street", "10000"));
-//
-//            member.getFavoriteFoods().add("치킨");
-//            member.getFavoriteFoods().add("족발");
-//            member.getFavoriteFoods().add("피자");
-//
-//            member.getAddressHistory().add(new AddressEntity("old1", "street", "10000"));
-//            member.getAddressHistory().add(new AddressEntity("old2", "street", "10000"));
-//
-//            em.persist(member);
-//
-//            em.flush();
-//            em.clear();
-//
-//            System.out.println("=============== START ====================");
-//
-//            Member findMember = em.find(Member.class, member.getId());
-//
-//            // homeCity -> newCity
-//            Address a = findMember.getHomeAddress();
-//            findMember.setHomeAddress(new Address("newCity", a.getStreet(), a.getZipcode()));
-//
-//            // 치킨 -> 한식
-//            findMember.getFavoriteFoods().remove("치킨");
-//            findMember.getFavoriteFoods().add("한식");
-            // 컬렉션의 값만 변경해도 실제 데이터베이스 쿼리가 날라가면서 JPA가 알아서 다 바꿔줌!! 영속성 전이처럼!
-            // delete쿼리 1개 나가고, insert쿼리 1개 들어옴
+            Member member = new Member();
+            member.setUsername("member1");
+            em.persist(member);
 
-            // old1 -> new1
-            // equals, hashcode 가 제대로 구현이 되어있어야 지워진다!!!!!!!!!!!!
-            // 값 타입이기 때문에 통으로 갈아야 되는 거 기억나지?
-//            findMember.getAddressHistory().remove(new Address("old1", "street", "10000"));
-//            findMember.getAddressHistory().add(new Address("newCity1", "street", "10000"));
-            // delete쿼리 1개 나가고, insert쿼리 2개 들어옴 이유가 뭐지? 값 타입 컬렉션의 제약사항
-
-            System.out.println("================ END ===================");
+            List<Member> resultList =
+                    em.createNativeQuery("select MEMBER_ID, city, street, zipcode, USERNAME from Member", Member.class)
+                            .getResultList();
+            for (Member member1 : resultList) {
+                System.out.println("member1 = " + member1);
+            }
 
             tx.commit();
         } catch (Exception e) {
--- src/main/java/hellojpa/MemberProduct.java
+++ src/main/java/hellojpa/MemberProduct.java
@@ -1,26 +1,26 @@
-//package hellojpa;
-//
-//import hellojpa.Member;
-//import hellojpa.Product;
-//
-//import javax.persistence.*;
-//import java.util.ArrayList;
-//import java.util.List;
-//
-//@Entity
-//public class MemberProduct {
-//
-//    @Id @GeneratedValue
-//    private Long id;
-//
-//    @ManyToOne
-//    @JoinColumn(name = "MEMBER_ID")
-//    private Member members;
-//
-//    @ManyToOne
-//    @JoinColumn(name = "PRODUCT_ID")
-//    private Product products;
-//
-//    private int count;
-//    private int price;
-//}
+package hellojpa;
+
+import hellojpa.Member;
+import hellojpa.Product;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class MemberProduct {
+
+    @Id @GeneratedValue
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "MEMBER_ID")
+    private Member members;
+
+    @ManyToOne
+    @JoinColumn(name = "PRODUCT_ID")
+    private Product products;
+
+    private int count;
+    private int price;
+}
